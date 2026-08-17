2763beeb5fa83b01b3dd8357127bbff9aff93ade
Axel Pauwels <axelpauwels.be@gmail.com>
2026-08-17 20:30:19 +0200

    Adding FlightSearchComponent in new domain 'ticketing'

---
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flight-search',
+  imports: [],
+  templateUrl: './flight-search.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class FlightSearch {}
