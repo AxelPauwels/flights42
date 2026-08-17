f81d2f4bfacb1bd6bc0aa6ef3ac936153b7d30dd
Axel Pauwels <axelpauwels.be@gmail.com>
2026-08-17 13:57:41 +0200

    Patching html and css with example files

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Navbar } from './shell/navbar/navbar';
+import { Sidebar } from './shell/sidebar/sidebar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Navbar, Sidebar],
   templateUrl: './app.html',
-  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('flights42');
