6b197c41f797a9a58d679bc6e377c38668b82297
Axel Pauwels <axelpauwels.be@gmail.com>
2026-08-17 13:45:13 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,8 +5,13 @@ import { RouterOutlet } from '@angular/router';
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('flights42');
+
+  protected updateTitle(): void {
+    this.title.set('Highly Sophisticated Flight App');
+    console.log('Title updated', this.title());
+  }
 }
