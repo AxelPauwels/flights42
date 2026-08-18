3edcebf8bf463ae294e6c1fdd641178d71270796
Axel Pauwels <axelpauwels.be@gmail.com>
2026-08-18 20:32:43 +0200

    Adding httpResource to fetch flights

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
@@ -2,8 +2,7 @@ import { JsonPipe, DatePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormField, form } from '@angular/forms/signals';
 import { Flight } from '../../data/flight';
-import { initialAircraft } from '../../data/aircraft';
-import { HttpClient } from '@angular/common/http';
+import { httpResource } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-search',
@@ -12,55 +11,40 @@ import { HttpClient } from '@angular/common/http';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightSearch {
-  private readonly http = inject(HttpClient);
   protected readonly filter = signal({
     from: 'Hamburg',
     to: 'Graz',
   });
+  protected readonly flightsResource = httpResource<Flight[]>(
+    () => {
+      const filter = this.filter();
+      if (!filter.from || !filter.to) {
+        return undefined;
+      }
+
+      return {
+        url: 'https://demo.angulararchitects.io/api/flight',
+        params: {
+          from: filter.from,
+          to: filter.to,
+        },
+      };
+    },
+    { defaultValue: [] },
+  );
+
+  // Get resource result and status
+  protected readonly flights = this.flightsResource.value;
+  protected readonly error = this.flightsResource.error;
+  protected readonly isLoading = this.flightsResource.isLoading;
+
   protected readonly filterForm = form(this.filter);
-  protected readonly flights = signal<Flight[]>([]);
   protected readonly selectedFlight = signal<Flight | null>(null);
   protected search(): void {
-    const url = 'https://demo.angulararchitects.io/api/flight';
-    const filter = this.filter();
-    const params = {
-      from: filter.from,
-      to: filter.to,
-    };
-    this.http.get<Flight[]>(url, { params }).subscribe({
-      next: (flights) => {
-        this.flights.set(flights);
-      },
-      error: (err) => {
-        console.error('Error', err);
-      },
-    });
+    this.flightsResource.reload();
   }
 
   protected select(f: Flight): void {
     this.selectedFlight.set(f);
-  }
-
-  createDemoFlight(): void {
-    const url = 'https://demo.angulararchitects.io/api/flight';
-    const newFlight: Flight = {
-      id: 0,
-      from: 'Gleisdorf',
-      to: 'Graz',
-      date: new Date().toISOString(),
-      delayed: false,
-      delay: 0,
-      aircraft: { ...initialAircraft },
-      prices: [],
-    };
-
-    this.http.post<Flight>(url, newFlight).subscribe({
-      next: (flight) => {
-        console.debug('New Id: ', flight.id);
-      },
-      error: (err) => {
-        console.error('Error', err);
-      },
-    });
   }
 }
