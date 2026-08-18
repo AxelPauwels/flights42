558775f2a23367e472761fdc4f0dbbec28df10a0
Axel Pauwels <axelpauwels.be@gmail.com>
2026-08-18 20:18:23 +0200

    Adding httpClient to fetch flights

--- src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
+++ src/app/domains/ticketing/feature-booking/flight-search/flight-search.ts
@@ -1,8 +1,9 @@
 import { JsonPipe, DatePipe } from '@angular/common';
-import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, signal } from '@angular/core';
 import { FormField, form } from '@angular/forms/signals';
 import { Flight } from '../../data/flight';
 import { initialAircraft } from '../../data/aircraft';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-flight-search',
@@ -11,6 +12,7 @@ import { initialAircraft } from '../../data/aircraft';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FlightSearch {
+  private readonly http = inject(HttpClient);
   protected readonly filter = signal({
     from: 'Hamburg',
     to: 'Graz',
@@ -19,42 +21,46 @@ export class FlightSearch {
   protected readonly flights = signal<Flight[]>([]);
   protected readonly selectedFlight = signal<Flight | null>(null);
   protected search(): void {
-    const date = new Date().toISOString();
-    this.flights.set([
-      {
-        id: 1,
-        from: this.filter().from,
-        to: this.filter().to,
-        date,
-        delayed: false,
-        delay: 0,
-        aircraft: { ...initialAircraft },
-        prices: [],
+    const url = 'https://demo.angulararchitects.io/api/flight';
+    const filter = this.filter();
+    const params = {
+      from: filter.from,
+      to: filter.to,
+    };
+    this.http.get<Flight[]>(url, { params }).subscribe({
+      next: (flights) => {
+        this.flights.set(flights);
       },
-      {
-        id: 2,
-        from: this.filter().from,
-        to: this.filter().to,
-        date,
-        delayed: false,
-        delay: 0,
-        aircraft: { ...initialAircraft },
-        prices: [],
+      error: (err) => {
+        console.error('Error', err);
       },
-      {
-        id: 3,
-        from: this.filter().from,
-        to: this.filter().to,
-        date,
-        delayed: false,
-        delay: 0,
-        aircraft: { ...initialAircraft },
-        prices: [],
-      },
-    ]);
+    });
   }
 
   protected select(f: Flight): void {
     this.selectedFlight.set(f);
+  }
+
+  createDemoFlight(): void {
+    const url = 'https://demo.angulararchitects.io/api/flight';
+    const newFlight: Flight = {
+      id: 0,
+      from: 'Gleisdorf',
+      to: 'Graz',
+      date: new Date().toISOString(),
+      delayed: false,
+      delay: 0,
+      aircraft: { ...initialAircraft },
+      prices: [],
+    };
+
+    this.http.post<Flight>(url, newFlight).subscribe({
+      next: (flight) => {
+        console.debug('New Id: ', flight.id);
+      },
+      error: (err) => {
+        console.error('Error', err);
+      },
+    });
   }
 }
