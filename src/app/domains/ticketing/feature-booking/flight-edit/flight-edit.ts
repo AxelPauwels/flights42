1788acdd1922e8a0c163e5d038c0082b8a4ef9f2
Axel Pauwels <axel.pauwels@ravago.be>
2026-09-16 09:33:02 +0200

    Adding activatedRoute listeners in ts code

--- src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.ts
+++ src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.ts
@@ -17,17 +17,18 @@ import {
 })
 export class FlightEdit {
   // private readonly route = inject(ActivatedRoute);
+  private readonly activatedRoute = inject(ActivatedRoute);
 
   // protected readonly id = signal(0);
   // protected readonly showDetails = signal(false);
 
   // constructor() {
-    // this.route.paramMap.subscribe((paramsMap) => {
-    //   const flightId = parseInt(paramsMap.get('id') ?? '0');
-    //   this.id.set(flightId);
-    //   const showDetails = (paramsMap.get('showDetails') === 'true');
-    //   this.showDetails.set(showDetails);
-    // });
+  // this.route.paramMap.subscribe((paramsMap) => {
+  //   const flightId = parseInt(paramsMap.get('id') ?? '0');
+  //   this.id.set(flightId);
+  //   const showDetails = (paramsMap.get('showDetails') === 'true');
+  //   this.showDetails.set(showDetails);
+  // });
   // }
 
   protected readonly id = input.required({
@@ -41,6 +42,16 @@ export class FlightEdit {
     effect(() => {
       console.log('id', this.id());
       console.log('showDetails', this.showDetails());
+    });
+
+    this.activatedRoute.queryParamMap.subscribe((queryParamMap) => {
+      // Key-value pairs of the query string
+      console.log('subscribe queryParamMap = ', queryParamMap);
+    });
+
+    this.activatedRoute.fragment.subscribe((fragment) => {
+      // One string representing the hash fragment
+      console.log('subscribe fragment = ', fragment);
     });
   }
 }
