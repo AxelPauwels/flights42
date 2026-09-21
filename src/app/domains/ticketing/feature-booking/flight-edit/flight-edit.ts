eae9f7879e2d4d0f22bc2c17c2a89fe949021474
Axel Pauwels <axel.pauwels@ravago.be>
2026-09-21 14:24:05 +0200

    Adding signal form flight edit

--- src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.ts
+++ src/app/domains/ticketing/feature-booking/flight-edit/flight-edit.ts
@@ -4,10 +4,13 @@ import {
   ChangeDetectionStrategy,
   Component, effect,
   inject,
-  input,
+  input, linkedSignal,
   numberAttribute,
   signal
 } from '@angular/core';
+import { SimpleFlightDetailStore } from './simple-flight-detail-store';
+import { Flight } from '../../data/flight';
+import { form, minLength, required } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-flight-edit',
@@ -18,6 +21,8 @@ import {
 export class FlightEdit {
   // private readonly route = inject(ActivatedRoute);
   private readonly activatedRoute = inject(ActivatedRoute);
+  private readonly store = inject(SimpleFlightDetailStore);
+  protected readonly flight = linkedSignal(() => normalizeFlight(this.store.flight()));
 
   // protected readonly id = signal(0);
   // protected readonly showDetails = signal(false);
@@ -31,6 +36,14 @@ export class FlightEdit {
   // });
   // }
 
+  // Set up the Signal Form with validation rules
+  protected readonly flightForm = form(this.flight, (path) => {
+    required(path.from);
+    required(path.to);
+    required(path.date);
+    minLength(path.from, 3);
+  });
+
   protected readonly id = input.required({
     transform: numberAttribute,
   });
@@ -38,20 +51,28 @@ export class FlightEdit {
     transform: booleanAttribute,
   });
 
-  constructor() {
-    effect(() => {
-      console.log('id', this.id());
-      console.log('showDetails', this.showDetails());
-    });
+  // constructor() {
+  // effect(() => {
+  //   console.log('id', this.id());
+  //   console.log('showDetails', this.showDetails());
+  // });
+  //
+  // this.activatedRoute.queryParamMap.subscribe((queryParamMap) => {
+  //   // Key-value pairs of the query string
+  //   console.log('subscribe queryParamMap = ', queryParamMap);
+  // });
+  //
+  // this.activatedRoute.fragment.subscribe((fragment) => {
+  //   // One string representing the hash fragment
+  //   console.log('subscribe fragment = ', fragment);
+  // });
+  // }
+}
 
-    this.activatedRoute.queryParamMap.subscribe((queryParamMap) => {
-      // Key-value pairs of the query string
-      console.log('subscribe queryParamMap = ', queryParamMap);
-    });
-
-    this.activatedRoute.fragment.subscribe((fragment) => {
-      // One string representing the hash fragment
-      console.log('subscribe fragment = ', fragment);
-    });
-  }
+function normalizeFlight(flight: Flight): Flight {
+  const localDate = flight.date.substring(0, 16);
+  return {
+    ...flight,
+    date: localDate,
+  };
 }
