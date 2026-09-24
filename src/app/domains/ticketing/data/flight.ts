28db5c4a7454377ec6420b984af4ea1483565a18
Axel Pauwels <axel.pauwels@ravago.be>
2026-09-24 11:22:33 +0200

    Adding conditional validators to schema like appplyWhenVlue,applyWhen, and required with 'when' config

--- src/app/domains/ticketing/data/flight.ts
+++ src/app/domains/ticketing/data/flight.ts
@@ -1,14 +1,15 @@
 import { Aircraft, initialAircraft } from './aircraft';
 import { Price } from './price';
 import {
-  apply,
+  apply, applyWhenValue,
   disabled,
   hidden,
   minLength,
   readonly,
   required,
-  schema, validate,
-  validateStandardSchema
+  min,
+  schema,
+  validateStandardSchema, applyWhen
 } from '@angular/forms/signals';
 import { FlightZodSchema, validateWithFlightSchema } from './flight-zod-schema';
 import { signal } from '@angular/core';
@@ -36,11 +37,29 @@ export const initialFlight: Flight = {
   prices: [],
 };
 
+export const delayedFlight = schema<Flight>((path) => {
+  required(path.delay);
+  min(path.delay, 15);
+});
+
+
 export const flightSchema = schema<Flight>((path) => {
   required(path.from);
   required(path.to);
   required(path.date);
   minLength(path.from, 3);
+
+  // When the predicate is true, the schema 'delayedFlight' will be applied
+  applyWhenValue(path, (flight) => flight.delayed, delayedFlight);
+
+  // The predicate receives the current context, which provides access to the entire field state
+  // Could also be ctx.stateOf() that returns the entire field state to check properties such as dirty for example
+  // applyWhen(path, (ctx) => ctx.valueOf(path.delayed), delayedFlight);
+
+  // another approach
+  // required(path.delay, {
+  //   when: (ctx) => ctx.valueOf(path.delayed),
+  // });
 });
 
 // create schema based on schema
